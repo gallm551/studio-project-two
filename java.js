de63819128464d222664af2c1ed5df865eb18ca2
gallm551 <gallm551@newschool.edu>
2019-02-13 17:15:36 -0500

    commit pls & thx

---
+++ java.js
@@ -0,0 +1,9 @@
+/* Comments */
+// hello
+
+
+var currentTime = new Date().getHours();
+
+      if (8 <= currentTime&&currentTime < 6) {
+       window.location.href = "indexsleeptime.html"; 
+     }
